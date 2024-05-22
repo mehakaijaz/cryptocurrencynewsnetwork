c30c0b9705ffe1a9d01996b381917744f88df74b
mehak aijaz <mehakaijaz716@gmail.com>
2024-05-22 21:35:09 +0530

    initial commit

---
+++ cryptocurrencynewsnetwork/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for cryptocurrencynewsnetwork project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cryptocurrencynewsnetwork.settings')
+
+application = get_asgi_application()
