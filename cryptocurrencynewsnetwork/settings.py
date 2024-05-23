1665da579fc17dd6a3e5ecc9afdf5b4298fb10d1
mehak aijaz <mehakaijaz716@gmail.com>
2024-05-23 23:13:10 +0530

    vercel deployment

--- cryptocurrencynewsnetwork/settings.py
+++ cryptocurrencynewsnetwork/settings.py
@@ -63,13 +63,7 @@ WSGI_APPLICATION = 'cryptocurrencynewsnetwork.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+DATABASES = {}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
